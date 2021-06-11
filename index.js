ab21d05166c993d5c3e02f5f86bc9710a2351a57
Harsh Rajput <hrrajput20@gmail.com>
2021-06-11 19:10:37 +0530

    add views dir

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view-engine', 'ejs');
+
+
+
+
+
 
 app.get('/', (req, res) => {
-    res.send('Home Page');
+    res.render('index.ejs')
+})
+
+app.get('*', (req, res) => {
+    res.send('invalid PATH request');
 })
 
 app.listen(3000, () => {
