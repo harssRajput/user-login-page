3b5a7dc518b9b2e816ab12ac9267849afc8edfdf
Harsh Rajput <hrrajput20@gmail.com>
2021-06-11 19:18:31 +0530

    add register and login pages

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view-engine', 'ejs');
 
 
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+})
 
-
-
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+})
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name : 'anonymous'});
